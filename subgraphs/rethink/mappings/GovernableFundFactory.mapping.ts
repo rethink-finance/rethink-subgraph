42d48d67f3e9a19f13202a511069a63f7ab3911f
L. E <luca@soonami.io>
2024-10-31 14:38:14 +0100

    feat: index governanceToken as VotingContract

--- subgraphs/rethink/mappings/GovernableFundFactory.mapping.ts
+++ subgraphs/rethink/mappings/GovernableFundFactory.mapping.ts
@@ -1,18 +1,21 @@
 import { transactions } from "@amxx/graphprotocol-utils"
-import { GovernableFund, Governor } from "../generated/schema"
+import { GovernableFund, Governor, VotingContract } from "../generated/schema"
 
 import { Address } from "@graphprotocol/graph-ts"
 import { GovernableFundFactory_CreatedAndInitializedFundEvent } from "../generated/GovernableFundFactory/GovernableFundFactory"
-import { RethinkFundGovernor } from "../generated/templates"
+import { ERC20Votes, RethinkFundGovernor } from "../generated/templates"
 import { fetchGovernor } from "../utils/RethinkFundGovernor.util"
+import { fetchVoting } from "../utils/Voting.util"
 
 export function handleGovernableFundFactory_CreatedAndInitializedFundEvent(
 	event: GovernableFundFactory_CreatedAndInitializedFundEvent,
 ): void {
 	let governor = createGovernorTemplate(event.params.govContractAddr)
+	let voting = createVotingTemplate(event.params.governanceToken)
 	let fund = new GovernableFund(event.params.fundContractAddr)
 	fund.transaction = transactions.log(event).id
 	fund.governor = governor.id
+	fund.votingContract = voting.id
 	fund.fundName = event.params.fundName
 	fund.fundContractAddr = event.params.fundContractAddr
 	fund.govContractAddr = event.params.govContractAddr
@@ -26,4 +29,10 @@ function createGovernorTemplate(governorAddress: Address): Governor {
 	RethinkFundGovernor.create(governorAddress)
 	let governor = fetchGovernor(governorAddress)
 	return governor
+}
+
+function createVotingTemplate(votingAddress: Address): VotingContract {
+	ERC20Votes.create(votingAddress)
+	let voting = fetchVoting(votingAddress)
+	return voting
 }
